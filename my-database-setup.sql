1c5de503084a983d6819d49eabc30c3d07c6451b
simonhall-scottlogic <simonhall-scottlogic@users.noreply.github.com>
2016-10-16 17:27:51 +0100

    Added configuration of node version and db with circle.yml

---
+++ my-database-setup.sql
@@ -0,0 +1,5 @@
+create table simple_table (label varchar, val integer);
+
+insert into simple_table (label, val) values ('first_label', 10);
+insert into simple_table (label, val) values ('second_label', 12);
+
