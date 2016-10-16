be83251da7289b9ee4ab40100c5696f00b3ced61
simonhall-scottlogic <simonhall-scottlogic@users.noreply.github.com>
2016-10-16 17:41:56 +0100

    adding a length to my varchar

--- my-database-setup.sql
+++ my-database-setup.sql
@@ -1,4 +1,4 @@
-create table simple_table (label varchar, val integer);
+create table simple_table (label varchar(32), val integer);
 
 insert into simple_table (label, val) values ('first_label', 10);
 insert into simple_table (label, val) values ('second_label', 12);
